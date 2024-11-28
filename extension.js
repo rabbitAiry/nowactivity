fe3640350be8cec49a2ff9fcd21540db5b1b7728
airy <1264889738@qq.com>
2024-11-28 21:00:05 +0800

    1.1

--- extension.js
+++ extension.js
@@ -1,4 +1,5 @@
 import St from 'gi://St';
+import Gio from "gi://Gio";
 import GLib from 'gi://GLib';
 import Clutter from 'gi://Clutter';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
@@ -24,32 +25,28 @@ export default class NowActivityExtension extends Extension {
 
     disable() {
         this.isEnable = false;
-        this._indicator?.destroy();
-        this._indicator = null;
+        this.indicator?.destroy();
+        this.indicator = null;
     }
 
     initIndicator() {
-        this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
-        this._icon = new St.Icon({ y_align: Clutter.ActorAlign.CENTER, style_class: 'system-status-icon', icon_name: "today-symbolic" });
-        this._label = new St.Label({ y_align: Clutter.ActorAlign.CENTER, style_class: 'system-status-label' });
-        let layout = new St.BoxLayout({ y_align: Clutter.ActorAlign.CENTER, style_class: 'panel-status-menu-box' });
-        layout.add_child(this._icon);
-        layout.add_child(this._label);
-        this._indicator.add_child(layout);
-        this._indicator.connect('button-press-event', () => this.getNowActivity());
-        Main.panel._addToPanelBox('NowActivity', this._indicator, -1, Main.panel._leftBox);
+        this.iconIdle = Gio.icon_new_for_string(this.path + "/res/tea.svg");
+        this.iconDelay = Gio.icon_new_for_string(this.path + "/res/delay.svg");
+
+        this.indicator = new PanelMenu.Button(0.5, this.metadata.name, false);
+        this.icon = new St.Icon({ y_align: Clutter.ActorAlign.CENTER, style_class: 'system-status-icon'});
+        this.label = new St.Label({ y_align: Clutter.ActorAlign.CENTER, style_class: 'system-status-label' });
+        let layout = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
+        layout.add_child(this.icon);
+        layout.add_child(this.label);
+        this.indicator.add_child(layout);
+        this.indicator.connect('button-press-event', () => this.getNowActivity());
+        Main.panel._addToPanelBox('NowActivity', this.indicator, -1, Main.panel._leftBox);
     }
 
     patchEvents() {
         this.src = new Calendar.DBusEventSource();
         this.getNowActivity()
-
-        // let original = this.src._reloadEvents;
-        // let obj = this
-        // this.src._reloadEvents = function () {
-        //     original()
-        //     obj.getNowActivity()
-        // }
     }
 
     getNowActivity() {
@@ -62,41 +59,49 @@ export default class NowActivityExtension extends Extension {
         if (events.length == 0) {
             this.setNoActivityNow()
         } else {
-            let fullDayActivityList = []
-            let nowActivityList = []
+            let fullDayActivityList = [];
+            let nowActivityList = [];
+            let futureNextActivity = null;
+            let futureDiff = Number.MAX_VALUE;
             events.forEach(e => {
                 if (this.isFullDayActivity(dateStart, e)) {
                     fullDayActivityList.push(e);
                 } else if (this.isNowActivity(now, e)) {
                     nowActivityList.push(e);
+                } else {
+                    let diff = this.timeWaitingForActivity(now, e)
+                    if(diff > 0 && diff < futureDiff){
+                        futureNextActivity = e
+                        futureDiff = diff
+                    }
                 }
             });
-            this.setActivityText(fullDayActivityList, nowActivityList)
+            this.setActivityText(fullDayActivityList, nowActivityList, futureNextActivity)
         }
     }
 
-    setActivityText(fullDayActivityList, nowActivityList) {
-        if (fullDayActivityList.length == 0 && nowActivityList.length == 0) {
-            this.setNoActivityNow()
-        } else {
+    setActivityText(fullDayActivityList, nowActivityList, future) {
+        if (fullDayActivityList.length != 0 || nowActivityList.length != 0) {
             let fullDayTextList = fullDayActivityList.map(it => it.summary)
-            let nowTextList = nowActivityList.map(it => it.summary + " 🡪 " + this.dateToTimeText(it.end))
+            let nowTextList = nowActivityList.map(it => it.summary + " ➔ " + this.dateToTimeText(it.end))
             let textList = fullDayTextList.concat(nowTextList)
-            this.setText(textList.join("  |  "))
-
-            console.log("fullDayTextList")
-            console.log(fullDayTextList)
-            console.log("textList")
-            console.log(textList)
+            this.setText(textList.join("  |  "));
+            this.icon.icon_name = "today-symbolic";
+        } else if (future != null) {
+            this.setText(`   (${this.dateToTimeText(future.date)} ➔ ${future.summary})`);
+            this.icon.set_gicon(this.iconDelay);
+        } else {
+            this.setNoActivityNow();   
         }
     }
 
     setNoActivityNow() {
-        this.setText("")
+        this.setText("");
+        this.icon.set_gicon(this.iconIdle);
     }
 
     setText(text) {
-        this._label.text = text;
+        this.label.text = text;
     }
 
     isFullDayActivity(start, event) {
@@ -109,7 +114,11 @@ export default class NowActivityExtension extends Extension {
     }
 
     isNowActivity(now, event) {
-        return event.date <= now && now <= event.end
+        return event.date <= now && now <= event.end;
+    }
+
+    timeWaitingForActivity(now, event) {
+        return event.date.getTime() - now.getTime();
     }
 
     dateToTimeText(date) {
@@ -118,55 +127,3 @@ export default class NowActivityExtension extends Extension {
         return `${hour}:${minute}`;
     }
 }
-/*
-Array [
-    {
-        "id": "fdbcdda3fb9bb137df10e99b311f14011af8a039\n6coe68o3orh54spj70n405rl39@google.com\n",
-        "date": "2024-06-16T01:00:00.000Z",
-        "end": "2024-06-16T02:00:00.000Z",
-        "summary": "NowActivity项目"
-    },
-    {
-        "id": "fdbcdda3fb9bb137df10e99b311f14011af8a039\n6dscaj87ofte43tai2ro7ohrvn@google.com\n",
-        "date": "2024-06-16T03:00:00.000Z",
-        "end": "2024-06-16T04:00:00.000Z",
-        "summary": "雨前运动"
-    },
-    {
-        "id": "fdbcdda3fb9bb137df10e99b311f14011af8a039\n2sno5n8tk174n84es2nabp4qgg@google.com\n",
-        "date": "2024-06-16T06:00:00.000Z",
-        "end": "2024-06-16T07:00:00.000Z",
-        "summary": "高数计划"
-    },
-    {
-        "id": "fdbcdda3fb9bb137df10e99b311f14011af8a039\n79u3al67frlbe8oru78i5b2ik2@google.com\n",
-        "date": "2024-06-16T07:00:00.000Z",
-        "end": "2024-06-16T08:00:00.000Z",
-        "summary": "b26 处理器-异常、指令级并行"
-    },
-    {
-        "id": "fdbcdda3fb9bb137df10e99b311f14011af8a039\n6bie3rjn4eo9hhnqq4m6aj3jhe@google.com\n",
-        "date": "2024-06-16T08:00:00.000Z",
-        "end": "2024-06-16T09:00:00.000Z",
-        "summary": "b30 处理器-剩"
-    },
-    {
-        "id": "fdbcdda3fb9bb137df10e99b311f14011af8a039\n1ohmal7d0je5ivd7mdi0cjshh9@google.com\n",
-        "date": "2024-06-16T09:00:00.000Z",
-        "end": "2024-06-16T10:00:00.000Z",
-        "summary": "存储-预览"
-    },
-    {
-        "id": "fdbcdda3fb9bb137df10e99b311f14011af8a039\n6cjdmvb8d0ljl09s11audlnqbk@google.com\n",
-        "date": "2024-06-16T12:00:00.000Z",
-        "end": "2024-06-16T13:00:00.000Z",
-        "summary": "ml9 序列化APIs"
-    },
-    {
-        "id": "fdbcdda3fb9bb137df10e99b311f14011af8a039\n26pv5c8g2k9781ff83uajt3hej@google.com\n",
-        "date": "2024-06-16T13:00:00.000Z",
-        "end": "2024-06-16T14:00:00.000Z",
-        "summary": "ml10 NLP训练新闻分词"
-    }
-]
-*/
--- version.md
+++ version.md
@@ -1,3 +1,8 @@
+## 1
+#### 1.1
+- feature
+    - bug fixes
+
 #### 1.0
 - feature
     - show the current activity
